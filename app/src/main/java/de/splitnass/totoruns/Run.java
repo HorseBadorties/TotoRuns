b82b885a2787db33c2581981176f0895cf7680cb
HorseBadorties <no@thanks.com>
2017-07-21 10:23:17 +0200

    Commit 2

--- app/src/main/java/de/splitnass/totoruns/Run.java
+++ app/src/main/java/de/splitnass/totoruns/Run.java
@@ -108,7 +108,6 @@ public class Run {
 
     public String getDurationString() {
         long seconds = getDuration()/1000;
-
         return String.format(
                 "%d:%02d:%02d",
                 seconds / 3600,
